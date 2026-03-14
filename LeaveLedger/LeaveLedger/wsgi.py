679e651d7b5a4ea26480976bcafb55b09d3deab6
angelmerin1307 <angelmerin1307@gmail.com>
2026-03-14 22:16:01 +0530

    Fix Django settings path

--- LeaveLedger/LeaveLedger/wsgi.py
+++ LeaveLedger/LeaveLedger/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LeaveLedger.LeaveLedger.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LeaveLedger.settings')
 
 application = get_wsgi_application()
