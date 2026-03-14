dbe9fd3ad1eba708e3fa2d414b2a43f1176c8959
angelmerin1307 <angelmerin1307@gmail.com>
2026-03-14 21:03:12 +0530

    Update asgi.py

--- LeaveLedger/LeaveLedger/asgi.py
+++ LeaveLedger/LeaveLedger/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LeaveLedger.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LeaveLedger.LeaveLedger.settings')
 
 application = get_asgi_application()
